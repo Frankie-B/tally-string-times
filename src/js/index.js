9888e8244525ae420be2df0a964d5146dd8b7b99
Frankie Bukenya <frankie.bukenya@gmail.com>
2019-06-08 13:54:57 +0300

    Convert times from strings to number arrays

--- src/js/index.js
+++ src/js/index.js
@@ -2,3 +2,9 @@
 
 const timeNodes = Array.from(document.querySelectorAll('[data-time]'));
 
+const seconds = timeNodes
+  .map(node => node.dataset.time)
+  .map(timeCode => {
+    const [mins, secs] = timeCode.split(':').map(parseFloat);
+    console.log(mins, secs);
+  });
